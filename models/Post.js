5a073548f0e2face7044d46e7f87d63e1db789d6
wookyungseo <dnrudtj9949@naver.com>
2024-03-12 11:37:02 +0900

    schema update

---
+++ models/Post.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  postTitle:{
+    type:String
+  },
+
+  postContent:{
+    type:String,
+    required:true
+  },
+
+  writer:{
+    type:mongoose.Schema.Types.ObjectId,
+    required:true
+  },
+
+  comments: [
+    {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Comment",
+    },
+  ],
+
+  createdAt:{
+    type:Date,
+    default:Date.now,
+  },
+
+  views:{
+    type:Number, default: 0
+  },
+  
+  recommend:[
+    {
+    type: Number,
+    defalut: 0
+    },
+  ]
+})
+
+const Post = mongoose.model("Post", PostSchema);
+  
+module.exports = Post;
