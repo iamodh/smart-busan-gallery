7e2bb7de21c3a8ac02ee22e25e8c088c8c7576e7
wookyungseo <dnrudtj9949@naver.com>
2024-03-12 11:37:02 +0900

    schema update

---
+++ models/Users.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  username:{
+    type:String,
+    required:true,
+    unique:true
+  },
+
+  password:{
+    type:String,
+    required:true
+  },
+
+  posts:[
+    {
+    type:mongoose.Schema.Types.ObjectId,
+    ref: "Post"
+    }
+  ],
+
+  comments:[
+    {
+    type:mongoose.Schema.Types.ObjectId,
+    ref: "Comment"
+    }
+  ],
+
+});
+
+const User = mongoose.model("User", UserSchema);
+  
+module.exports = User;
