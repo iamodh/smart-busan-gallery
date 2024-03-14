94b5a329c4e77195ec805d8902768dae0d88c874
broisHyeong <limminhyeong@gmail.com>
2024-03-14 12:08:06 +0900

    create postview_lmh

--- public/js/postview.js
+++ public/js/postview.js
@@ -7,3 +7,11 @@ function submitComment() {
     commentList.appendChild(newComment); // 새 댓글을 댓글 리스트에 추가합니다.
     commentInput.value = ''; // 댓글 입력란을 비웁니다.
 }
+
+function updateCommentCount() {
+    var commentList = document.getElementById('commentList');
+    var comments = commentList.getElementsByClassName('comment');
+    var commentCount = comments.length; // 댓글 개수 계산
+    document.getElementById('commentCount').innerText = `[${commentCount}]`; // 댓글 수 업데이트
+}
+// 댓글을 추가하거나 불러올 때 updateCommentCount 함수를 호출합니다.
